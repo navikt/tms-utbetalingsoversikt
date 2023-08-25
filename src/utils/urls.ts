7f9a1afb41cd61254448ba66d4785973702a5858
Amirhossien Lotfi <amirhossien.lotfi@nav.no>
2023-08-25 15:30:07 +0200

    La inn sjekk på intern-url for isProduction

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,4 +1,4 @@
-const isProduction = window.location.href.includes("www.nav.no");
+const isProduction = window.location.href.includes("www.nav.no") || window.location.href.includes("www.intern.nav.no");
 const isDevelopment = window.location.href.includes("www.intern.dev.nav.no");
 
 export const getEnvironment = () => {
