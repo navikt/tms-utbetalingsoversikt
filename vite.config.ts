8280a7064c897543530d1e055b132f80e6729727
Amirhossien Lotfi <amirhossien.lotfi@nav.no>
2023-08-27 14:25:16 +0200

    Mindre bundle ved å hente react fra CDN

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { viteMockServe } from "vite-plugin-mock";
 import { terser } from "rollup-plugin-terser";
+import { rollupImportMapPlugin } from "rollup-plugin-import-map";
+import importmap from "./importmap.json" assert { type: "json" };
 
 export default defineConfig(({ command }) => ({
   plugins: [
@@ -10,6 +12,11 @@ export default defineConfig(({ command }) => ({
       mockPath: "mock",
       localEnabled: command === "serve",
     }),
+    {
+      ...rollupImportMapPlugin([importmap]),
+      enforce: "pre",
+      apply: "build",
+    },
     terser(),
   ],
   css: {
