c3ad51ec16899266bc9fed9f959e6e509886a333
Amirhossien Lotfi <amirhossien.lotfi@nav.no>
2023-09-26 14:56:46 +0200

    Fix filtrering på ytelse

--- src/utils/filterUtbetaling.ts
+++ src/utils/filterUtbetaling.ts
@@ -2,17 +2,27 @@ import { UtbetalingGroupProps } from "~components/utbetalingGroup/UtbetalingGrou
 import { SelectedYtelser } from "~store/filter";
 
 const filterUtbetalinger = (
-  utbetalinger: UtbetalingGroupProps[],
+  utbetalingGroups: UtbetalingGroupProps[],
   selectedYtelser: SelectedYtelser
 ) => {
   const showAll = Object.values(selectedYtelser).every(
     (value) => value === false
   );
-  if (showAll) return utbetalinger;
+  if (showAll) return utbetalingGroups;
 
-  return utbetalinger.filter((period) =>
-    period.utbetalinger.find((utbetaling) => selectedYtelser[utbetaling.ytelse])
+  const filteredGroups = utbetalingGroups.reduce(
+    (acc: UtbetalingGroupProps[], period: UtbetalingGroupProps) => {
+      const filteredPeriod = period.utbetalinger.filter(
+        (utbetaling) => selectedYtelser[utbetaling.ytelse]
+      );
+      if (filteredPeriod.length > 0) {
+        return [...acc, { ...period, utbetalinger: filteredPeriod }];
+      }
+      return acc;
+    },
+    []
   );
+  return filteredGroups;
 };
 
 export default filterUtbetalinger;
