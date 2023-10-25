c1584423e3ae00f50188a30bb7701834c49fdac1
Amirhossien Lotfi <amirhossien.lotfi@nav.no>
2023-10-25 14:04:24 +0200

    WIP lagge til printvennlig visning av utbetalinger i en periode

--- src/pages/landingsside/Landingsside.tsx
+++ src/pages/landingsside/Landingsside.tsx
@@ -6,21 +6,25 @@ import InfoAlertLegacyApp from "~components/infoAlertLegacyApp/InfoAlertLegacyAp
 import RelatertInnhold from "~components/relatertInnhold/RelatertInnhold.tsx";
 import Utbetalinger from "~components/utbetalinger/Utbetalinger.tsx";
 import style from "./Landingsside.module.css";
+import PrintUtbetalinger from "./PrintUtbetalinger/PrintUtbetalinger";
 
 function Landingsside() {
   return (
     <>
-      <Breadcrumbs showUtbetalinger={false} />
-      <Heading className={style.pageTitle} level="1" size="xlarge">
-        Utbetalinger
-      </Heading>
-      <div className={style.pageBody}>
-        <ShowFilterButton />
-        <PeriodeFilter />
-        <Utbetalinger />
-        <InfoAlertLegacyApp />
-        <RelatertInnhold />
+      <div id={style.printHide}>
+        <Breadcrumbs showUtbetalinger={false} />
+        <Heading className={style.pageTitle} level="1" size="xlarge">
+          Utbetalinger
+        </Heading>
+        <div className={style.pageBody}>
+          <ShowFilterButton />
+          <PeriodeFilter />
+          <Utbetalinger />
+          <InfoAlertLegacyApp />
+          <RelatertInnhold />
+        </div>
       </div>
+      <PrintUtbetalinger />
     </>
   );
 }
---
+++ src/pages/landingsside/PrintUtbetalinger/PrintUtbetalinger.tsx
@@ -0,0 +1,53 @@
+import { useStore } from "@nanostores/react";
+import { BodyShort, Heading } from "@navikt/ds-react";
+import dayjs from "dayjs";
+import useSWR from "swr";
+import { periodeFilterAtom } from "~store/filter";
+import { utbetalingerAPIUrl } from "~utils/urls";
+import { formaterTallUtenDesimaler } from "~utils/utbetalingDetalje";
+import { fetcher } from "../../../api/api";
+import styles from "./PrintUtbetalinger.module.css";
+import logo from "./nav-logo.png";
+
+//TODO legge inn navn og fnr
+const PrintUtbetalinger = () => {
+  const selectedPeriodFilter = useStore(periodeFilterAtom);
+  const periodFom = dayjs(selectedPeriodFilter.fom).format("DD.MM.YYYY");
+  const periodTom = dayjs(selectedPeriodFilter.tom).format("DD.MM.YYYY");
+  const { data: utbetalinger } = useSWR(
+    {
+      path: utbetalingerAPIUrl(`?fom=${selectedPeriodFilter.fom}&tom=${selectedPeriodFilter.tom}`),
+    },
+    fetcher,
+    {
+      shouldRetryOnError: false,
+    }
+  );
+
+  const utbetalingerGroups = utbetalinger?.tidligere;
+
+  return (
+    <div id={styles.container}>
+      <img src={logo} alt="Logo" />
+      <BodyShort className={styles.pageTitle}>UTBETALINGSOVERSIKT</BodyShort>
+      <BodyShort className={styles.name}>NAVN</BodyShort> 
+      <BodyShort className={styles.fnr}>FNR</BodyShort>
+      <BodyShort className={styles.date}>{`Utskrift fra ${dayjs().format("DD.MM.YYYY")}`}</BodyShort>
+      <BodyShort className={styles.utbetaltIperiode}>Utbetalt i periode</BodyShort>{" "}
+      <Heading className={styles.periodeDate}level="1" size="small">{`${periodFom} - ${periodTom}`}</Heading>
+      {utbetalingerGroups?.map((g) => {
+        return g.utbetalinger.map((u) => (
+          <div className={styles.utbetaling}>
+            <Heading level="2" size="small" className={styles.utbetalingHeading}>
+              <span className={styles.ytelse}>{u.ytelse}</span>
+              <span className={styles.beløp}>{formaterTallUtenDesimaler(u.beløp) + " kr"}</span>
+            </Heading>
+            <BodyShort className={styles.utbetalingDato}>{`Utbetalt ${dayjs(u.dato).format("DD.MM.YYYY")}`}</BodyShort>
+          </div>
+        ));
+      })}
+    </div>
+  );
+};
+
+export default PrintUtbetalinger;
