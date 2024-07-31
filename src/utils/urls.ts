82a85cd2b0509d08cf9db305a162ba1f379eb06e
Sergio A. Arevalo Soria <sergio.alberto.arevalo.soria@nav.no>
2024-07-31 15:57:09 +0200

    Legg til ansatt url

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -3,10 +3,15 @@ const isProduction =
   window.location.href.includes("www.intern.nav.no") ||
   window.location.href.includes("tjenester.nav.no");
 const isDevelopment = window.location.href.includes("www.intern.dev.nav.no");
+const isAnsatt = window.location.href.includes("www.ansatt.dev.nav.no");
 
 export const getEnvironment = () => {
   if (isProduction) {
     return "production";
+  }
+
+  if (isAnsatt) {
+    return "ansatt";
   }
 
   if (isDevelopment) {
@@ -18,30 +23,35 @@ export const getEnvironment = () => {
 
 const BASE_URL = {
   local: "http://localhost:3000/utbetalingsoversikt/",
+  ansatt: "https://www.ansatt.dev.nav.no/utbetalingsoversikt",
   development: "https://www.intern.dev.nav.no/utbetalingsoversikt",
   production: "https://www.nav.no/utbetalingsoversikt",
 };
 
 const MIN_SIDE_URL = {
   local: "http://localhost:3000/minside",
+  ansatt: "https://www.ansatt.dev.nav.no/minside",
   development: "https://www.intern.dev.nav.no/minside",
   production: "https://www.nav.no/minside",
 };
 
 const UTBETALINGSOVERSIKT_API_URL = {
   local: "http://localhost:3000/api",
+  ansatt: "https://www.ansatt.dev.nav.no/tms-utbetalingsoversikt-api",
   development: "https://www.intern.dev.nav.no/tms-utbetalingsoversikt-api",
   production: "https://person.nav.no/tms-utbetalingsoversikt-api",
 };
 
 const SATSERURL = {
   local: "http://localhost:3000/satser",
+  ansatt: "https://www.intern.dev.nav.no/no/nav-og-samfunn/kontakt-nav/oversikt-over-satser",
   development: "https://www.intern.dev.nav.no/no/nav-og-samfunn/kontakt-nav/oversikt-over-satser",
   production: "https://www.nav.no/no/nav-og-samfunn/kontakt-nav/oversikt-over-satser",
 };
 
 const UTBETALINGSDATOER_URL = {
   local: "http://localhost:3000/utbetalignsdatoer",
+  ansatt: "https://www.intern.dev.nav.no/no/NAV+og+samfunn/Kontakt+NAV/Utbetalinger/Utbetalinger/Utbetalingsdatoer%2C+feriepenger+og+skattetrekk",
   development:
     "https://www.intern.dev.nav.no/no/NAV+og+samfunn/Kontakt+NAV/Utbetalinger/Utbetalinger/Utbetalingsdatoer%2C+feriepenger+og+skattetrekk",
   production:
@@ -50,12 +60,14 @@ const UTBETALINGSDATOER_URL = {
 
 const ENDRE_KONTONUMMER_URL = {
   local: "http://localhost:3000/kontonummer",
+  ansatt: "https://www.intern.dev.nav.no/kontonummer",
   development: "https://www.intern.dev.nav.no/kontonummer",
   production: "https://www.nav.no/kontonummer",
 };
 
 const ENDRE_SKATTEKORT_URL = {
   local: "http://localhost:300/skattekort",
+  ansatt: "https://www.intern.dev.nav.no/no/NAV+og+samfunn/Kontakt+NAV/Utbetalinger/skattetrekk-p%C3%A5-ytelser-fra-nav",
   development:
     "https://www.intern.dev.nav.no/no/NAV+og+samfunn/Kontakt+NAV/Utbetalinger/skattetrekk-p%C3%A5-ytelser-fra-nav",
   production: "https://www.nav.no/no/NAV+og+samfunn/Kontakt+NAV/Utbetalinger/skattetrekk-p%C3%A5-ytelser-fra-nav",
@@ -63,36 +75,42 @@ const ENDRE_SKATTEKORT_URL = {
 
 const SOSIALHJELP_URL = {
   local: "https://www.nav.no/sosialhjelp/innsyn/utbetaling",
+  ansatt: "https://www.intern.dev.nav.no/sosialhjelp/innsyn/utbetaling",
   development: "https://www.intern.dev.nav.no/sosialhjelp/innsyn/utbetaling",
   production: "https://www.nav.no/sosialhjelp/innsyn/utbetaling",
 };
 
 const DAGPENGER_URL = {
   local: "https://www.nav.no/dagpenger/forskudd/oversikt",
+  ansatt: "https://www.intern.dev.nav.no/dagpenger/forskudd/oversikt",
   development: "https://www.intern.dev.nav.no/dagpenger/forskudd/oversikt",
   production: "https://www.nav.no/dagpenger/forskudd/oversikt",
 };
 
 const ÅRSOPPGAVER_URL = {
   local: "https://www.nav.no/dokumentarkiv/tema/STO",
+  ansatt: "https://intern.dev.nav.no/dokumentarkiv/tema/STO",
   development: "https://intern.dev.nav.no/dokumentarkiv/tema/STO",
   production: "https://www.nav.no/dokumentarkiv/tema/STO",
 };
 
 const OM_UTBETALINGER_URL = {
   local: "http://localhost:3000/om-utbetalinger",
+  ansatt: "https://www.intern.dev.nav.no/utbetalinger",
   development: "https://www.intern.dev.nav.no/utbetalinger",
   production: "https://www.nav.no/utbetalinger",
 };
 
 const LEGACY_URL = {
   local: "http://localhost:3000/utbetalingsoversikt",
+  ansatt: "https://www.intern.dev.nav.no/utbetalingsoversikt-gammel",
   development: "https://www.intern.dev.nav.no/utbetalingsoversikt-gammel",
   production: "https://www.nav.no/utbetalingsoversikt-gammel",
 };
 
 const IDENT_NAVN_URL = {
   local: "http://localhost:3000/tms-min-side-proxy/navn",
+  ansatt: "https://www.intern.dev.nav.no/tms-min-side-proxy/navn",
   development: "https://www.intern.dev.nav.no/tms-min-side-proxy/navn",
   production: "https://www.nav.no/tms-min-side-proxy/navn",
 };
